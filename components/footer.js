2be1b30ecce949e2e3dbbb0c4e75f227f05a6b0d
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-07-10 00:06:58 -0400

    Se Ajustó los enlaces del header y footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,30 @@
-import Navegacion from "./navegacion";
+import Link from "next/link";
 import style from '@/styles/footer.module.css'
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className={style.footer}>
         <div className={`contenedor ${style.contenido}`}>
-            <Navegacion />
+        <nav className={style.navegacion}>
+            <Link
+                href="/"
+            >Inicio</Link>
+            <Link
+                href="/nosotros"
+            >Nosotros</Link>
+            <Link
+                href="/guitarras"
+            >Tienda</Link>
+            <Link
+                href="/blog"
+            >Blog</Link>
+            <Link
+                href="/carrito"
+            >
+                <Image src="/img/carrito.png" alt="carrito de compras" width={30} height={30} />
+            </Link>
+        </nav>
 
             <p className={style.copyright}>Todos los derechos reservados { new Date().getFullYear() } </p>
         </div>
--- components/header.js
+++ components/header.js
@@ -1,17 +1,43 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import style from '@/styles/header.module.css'
-import Navegacion from './navegacion'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+
+  const router = useRouter()
+
   return (
     <header className={style.header}>
     <div className={`contenedor ${style.barra}`}>
         <Link href="/">
-            <Image className='logo' src="/img/logo.svg" width={300} height={40} alt="Imagen logo" />
+            <Image className={style.logo} src="/img/logo.svg" width={300} height={40} alt="Imagen logo" />
         </Link>
         
-        <Navegacion />
+        <nav className={style.navegacion}>
+            <Link
+                href="/"
+                className={router.pathname === '/' ? style.active : '' }
+            >Inicio</Link>
+            <Link
+                href="/nosotros"
+                className={router.pathname === '/nosotros' ? style.active : '' }
+            >Nosotros</Link>
+            <Link
+                href="/guitarras"
+                className={router.pathname === '/tienda' ? style.active : '' }
+            >Tienda</Link>
+            <Link
+                href="/blog"
+                className={router.pathname === '/blog' ? style.active : '' }
+            >Blog</Link>
+            <Link
+                href="/carrito"
+                className={router.pathname === '/carrito' ? style.active : '' }
+            >
+                <Image src="/img/carrito.png" alt="carrito de compras" width={30} height={30} />
+            </Link>
+        </nav>
         
     </div>
     </header>   
