950b64a27e011262f61afdd6b42e8e886a70de26
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-07-10 00:23:14 -0400

    Se agregó el contenido de la página Nosotros y el estilo correspondiente

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout";
-import Link from "next/link";
+import Image from "next/image";
+import style from '@/styles/nosotros.module.css'
 
 export default function Nosotros() {
   return (
@@ -7,7 +8,23 @@ export default function Nosotros() {
       title={'Nosotros'}
       descripction="Venta de guitarras, blog de música"
     >
-      <h1>Nosotros</h1>
+    <main className="contenedor">
+      <h2 className="heading">Nosotros</h2>
+
+      <div className={style.contenido}>
+        <Image src="/img/nosotros.jpg" alt="imagen sobre nosotros" width={1000} height={800} priority={false} />
+
+        <div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto possimus sunt obcaecati molestias eos excepturi, illo asperiores eius atque similique magnam odio modi. At dolores, excepturi itaque alias et repudiandae, Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aliquid repellat ut veritatis repellendus odit, consequatur quibusdam earum. Velit perspiciatis voluptates corporis eaque magni commodi consequuntur aspernatur ab quas adipisci!.
+          </p>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate corporis perspiciatis pariatur. Ut sint eveniet odit, est eligendi unde velit facilis beatae excepturi delectus ullam minima veniam molestiae, deleniti libero?. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quis quisquam blanditiis libero doloribus? Eos veniam earum, veritatis asperiores repellendus mollitia. Maxime libero tempore provident eligendi, molestiae nobis similique neque.
+          </p>
+        </div>
+      </div>
+    </main>
     </Layout>
   )
 }
