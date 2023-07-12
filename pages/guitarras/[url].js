918e9aa0017967f83f64ac51551fd7fa8aae80e1
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-07-11 23:55:22 -0400

    Se configuró el routing dinamico de la vista de guitarra con getServerSideProps()

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,10 +1,29 @@
 import Layout from "@/components/layout";
 
-export default function Producto({url}) {
+export default function Producto(datos) {
+
+  console.log('::: guitarra :::', datos)
 
   return (
     <Layout>
         [url]
     </Layout>
   )
+}
+
+export async function getServerSideProps({query: {url}}) {
+
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
+  const { data } = await respuesta.json()
+
+  console.log(data[0])
+  
+  return {
+    props: {
+      guitarra: {
+        id: data[0].id,
+        ...data[0].attributes
+      }
+    }
+  }
 }
